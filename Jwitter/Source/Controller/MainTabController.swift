0cf4b4a23012bc02cbe68685c255c37a5425155f
Jiseok97 <wltjr1426@naver.com>
2022-02-07 23:15:12 +0900

    프로젝트 SetUp

---
+++ Jwitter/Source/Controller/MainTabController.swift
@@ -0,0 +1,23 @@
+//
+//  MainTabController.swift
+//  Jwitter
+//
+//  Created by 이지석 on 2022/02/07.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - Functions
+
+}
