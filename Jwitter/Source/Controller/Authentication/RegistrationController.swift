307b6c999a32c8e02a44c3e67458f07f73dfa0a0
Jiseok97 <wltjr1426@naver.com>
2022-02-10 13:25:16 +0900

    [에러]이미지URL DB 추가 실패

--- Jwitter/Source/Controller/Authentication/RegistrationController.swift
+++ Jwitter/Source/Controller/Authentication/RegistrationController.swift
@@ -119,21 +119,34 @@ class RegistrationController: UIViewController {
         guard let fullname = fullnameTextField.text else { return }
         guard let username = usernameTextField.text else { return }
         
-        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
-            // 로그인 실패
-            if let error = error {
-                print("에러내역: \(error)")
-                return
+        guard let imageData = profileImage.jpegData(compressionQuality: 0.3) else { return }
+        let filename = NSUUID().uuidString
+        let storageRef = STORAGE_PROFILE_IMAGES.child(filename)
+        
+        // 유저 이미지 및 회원 정보
+        storageRef.putData(imageData, metadata: nil) {(meta, error) in
+            storageRef.downloadURL { (url, error) in
+                guard let profileImageUrl = url?.absoluteString else { return }
+                
+                Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+                    // 로그인 실패
+                    if let error = error {
+                        print("에러내역: \(error.localizedDescription)")
+                        return
+                    }
+                    // 로그인 성공
+                    guard let uid = result?.user.uid else { return }    // User ID
+                    let values = [ "email": email,
+                                   "username": username,
+                                   "fullname": fullname,
+                                   "profileImageUrl" : profileImageUrl ]
+                    
+                    REF_USERS.child(uid).updateChildValues(values) { (error, ref) in
+                        print("DEBUG: 유저 정보 업데이트를 성공했습니다.")
+                    }
+                    
+                }
             }
-            
-            // 로그인 성공
-            guard let uid = result?.user.uid else { return }    // User ID
-            let values = [ "email": email, "username": username, "fullname": fullname ]
-            
-            REF_USERS.child(uid).updateChildValues(values) { (error, ref) in
-                print("DEBUG: 유저 정보 업데이트를 성공했습니다.")
-            }
-            
         }
     }
     
