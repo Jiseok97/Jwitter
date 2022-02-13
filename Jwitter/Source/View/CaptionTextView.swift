9cb5393a78dd6abd2d599c1a22486982cab51bbb
Jiseok97 <wltjr1426@naver.com>
2022-02-13 23:40:40 +0900

    [feat]텍스트필드 - 트윗컨트롤러

---
+++ Jwitter/Source/View/CaptionTextView.swift
@@ -0,0 +1,51 @@
+//
+//  CaptionTextView.swift
+//  Jwitter
+//
+//  Created by 이지석 on 2022/02/13.
+//
+
+import UIKit
+
+class CaptionTextView: UITextView {
+    
+    // MARK: - Properties
+    
+    let placeholderLable: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = .darkGray
+        label.text = "무슨 일이 일어나고 있나요?"
+        return label
+    }()
+    
+    
+    // MARK: - Life Cycle
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        
+        backgroundColor = .white
+        font = UIFont.systemFont(ofSize: 16)
+        isScrollEnabled = false
+        heightAnchor.constraint(equalToConstant: 300).isActive = true
+        
+        addSubview(placeholderLable)
+        placeholderLable.anchor(top: topAnchor, left: leftAnchor,
+                                paddingTop: 8, paddingLeft: 4)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleTextInputChange), name: UITextView.textDidChangeNotification, object: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    // MARK: - Selector
+    
+    @objc func handleTextInputChange() {
+        placeholderLable.isHidden = !text.isEmpty
+    }
+    
+}
