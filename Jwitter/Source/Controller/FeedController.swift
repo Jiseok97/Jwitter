d5e890e9a0fb87ba41ec484f2582c07524ea787e
Jiseok97 <wltjr1426@naver.com>
2022-02-14 20:50:32 +0900

    [feat]피드 셀 UI 작업

--- Jwitter/Source/Controller/FeedController.swift
+++ Jwitter/Source/Controller/FeedController.swift
@@ -86,6 +86,6 @@ extension FeedController {
 // MARK: - UICollectionView Delegate
 extension FeedController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 200)
+        return CGSize(width: view.frame.width, height: 120)
     }
 }
--- Jwitter/Source/View/TweetCell.swift
+++ Jwitter/Source/View/TweetCell.swift
@@ -9,14 +9,128 @@ import UIKit
 
 class TweetCell: UICollectionViewCell {
     
+    // MARK: - Properties
+    
+    private let profileImageView: UIImageView = {   // 유저 프로필 이미지
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.setDimensions(width: 48, height: 48)
+        iv.layer.cornerRadius = 48 / 2
+        iv.backgroundColor = .twitterBlue
+        return iv
+    }()
+    
+    private let captionLabel: UILabel = {           // 게시글 Label
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.numberOfLines = 0
+        label.text = "테스트 라벨.."
+        return label
+    }()
+    
+    private lazy var commentButton: UIButton = {    // 댓글 Button
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "comment"), for: .normal)
+        button.tintColor = .darkGray
+        button.setDimensions(width: 20, height: 20)
+        button.addTarget(self, action: #selector(handleCommentTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var retweetButton: UIButton = {    // 리트윗 Button
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "retweet"), for: .normal)
+        button.tintColor = .darkGray
+        button.setDimensions(width: 20, height: 20)
+        button.addTarget(self, action: #selector(handleRetweetTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var likeButton: UIButton = {       // 좋아요 Button
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "like"), for: .normal)
+        button.tintColor = .darkGray
+        button.setDimensions(width: 20, height: 20)
+        button.addTarget(self, action: #selector(handleLikeTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var shareButton: UIButton = {      // 공유 Button
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "share"), for: .normal)
+        button.tintColor = .darkGray
+        button.setDimensions(width: 20, height: 20)
+        button.addTarget(self, action: #selector(handleShareTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private let infoLabel = UILabel()             // 유저 닉네임 Label
+    
+    // MARK: - Life Cycle
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        // 프로필 이미지 Image View
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor,
+                                paddingTop: 8, paddingLeft: 8)
+        
+        // 유저 닉네임 및 게시글 Label
+        let stack = UIStackView(arrangedSubviews: [ infoLabel, captionLabel ])
+        stack.axis = .vertical
+        stack.distribution = .fillProportionally
+        stack.spacing = 4
+        
+        addSubview(stack)
+        stack.anchor(top: profileImageView.topAnchor, left: profileImageView.rightAnchor,
+                     right: rightAnchor, paddingLeft: 12, paddingRight: 12)
+        
+        infoLabel.font = UIFont.systemFont(ofSize: 14)
+        infoLabel.text = "Test User @username"
+        
+        // 댓글, 리트윗, 좋아요, 공유 버튼
+        let actionStack = UIStackView(arrangedSubviews: [ commentButton, retweetButton, likeButton, shareButton ])
+        actionStack.axis = .horizontal
+        actionStack.spacing = 72
+        
+        addSubview(actionStack)
+        actionStack.centerX(inView: self)
+        actionStack.anchor(bottom: bottomAnchor, paddingBottom: 8)
+        
+        // 셀 경계선
+        let underlineView = UIView()
+        underlineView.backgroundColor = .systemGroupedBackground
+        addSubview(underlineView)
+        underlineView.anchor(left: leftAnchor, bottom: bottomAnchor,
+                             right: rightAnchor, height: 1)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
+    // MARK: - Selectors
+    
+    @objc func handleCommentTapped() {
+        
+    }
+    
+    @objc func handleRetweetTapped() {
+        
+    }
+    
+    @objc func handleLikeTapped() {
+        
+    }
+    
+    @objc func handleShareTapped() {
+        
+    }
+    
+    
+    // MARK: - Functions
     
 }
