91e53c3893069378d042d4da8b5b573bf3bdd8ea
Jiseok97 <wltjr1426@naver.com>
2022-02-25 18:42:42 +0900

    [Add] 액션 시트 Window,TableView 추가

--- Jwitter/Source/Utils/ActionSheetLauncher.swift
+++ Jwitter/Source/Utils/ActionSheetLauncher.swift
@@ -5,22 +5,69 @@
 //  Created by 이지석 on 2022/02/25.
 //
 
-import Foundation
+import UIKit
+
+private let reuseIdentifier = "ActionSheetCell"
 
 class ActionSheetLauncher: NSObject {
     
     // MARK: - Properties
     
     private let user: User
+    private let tableView = UITableView()
+    private var window: UIWindow?                     // 기본적으로 Action Sheet도 하나의 창이기 때문에 생성
+                                                      // present처럼 보이기 원해서
+    
+    // MARK: - Life Cycle
     
     init(user: User) {
         self.user = user
         super.init()
+        
+        configureTableView()
     }
     
     // MARK: - Functinos
     
     func show() {
         print("DEBUG: Show action sheet for user \(user.username)")
+        
+        guard let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) else { return }
+        self.window = window
+        
+        window.addSubview(tableView)
+        tableView.frame = CGRect(x: 0, y: window.frame.height - 300, width: window.frame.width, height: 300)
     }
+    
+    func configureTableView() {
+        tableView.backgroundColor = .systemPurple
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 60
+        tableView.separatorStyle = .none
+        tableView.layer.cornerRadius = 5
+        tableView.isScrollEnabled = false
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension ActionSheetLauncher: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension ActionSheetLauncher: UITableViewDelegate {
+    
 }
