faeb7250171cd8f74a6350e7538389525f43d3df
Jiseok97 <wltjr1426@naver.com>
2022-02-25 22:38:36 +0900

    [feat] 액션 시트 닫기 버튼 추가

--- Jwitter/Source/Utils/ActionSheetLauncher.swift
+++ Jwitter/Source/Utils/ActionSheetLauncher.swift
@@ -17,7 +17,8 @@ class ActionSheetLauncher: NSObject {
     private let tableView = UITableView()
     private var window: UIWindow?                     // 기본적으로 Action Sheet도 하나의 창이기 때문에 생성
                                                       // present처럼 보이기 원해서
-    private lazy var blackView: UIView = {
+    
+    private lazy var blackView: UIView = {                                  // 액션 시트 올라올 때 뒷 배경 View
         let view = UIView()
         view.alpha = 0
         view.backgroundColor = UIColor(white: 0, alpha: 0.5)
@@ -26,6 +27,28 @@ class ActionSheetLauncher: NSObject {
         view.addGestureRecognizer(tap)
         
         return view
+    }()
+    
+    private lazy var footerView: UIView = {                                 // 닫기 버튼을 감쌀 View
+        let view = UIView()
+        
+        view.addSubview(cancelButton)
+        cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cancelButton.anchor(left: view.leftAnchor, right: view.rightAnchor, paddingLeft: 12, paddingRight: 12)
+        cancelButton.centerY(inView: view)
+        cancelButton.layer.cornerRadius = 50 / 2
+        
+        return view
+    }()
+    
+    private lazy var cancelButton: UIButton = {                             // 닫기 Button
+        let button = UIButton(type: .system)
+        button.setTitle("Cancel", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemGroupedBackground
+        button.addTarget(self, action: #selector(handleDismissal), for: .touchUpInside)
+        return button
     }()
     
     // MARK: - Life Cycle
@@ -44,6 +67,10 @@ class ActionSheetLauncher: NSObject {
             self.blackView.alpha = 0
             self.tableView.frame.origin.y += 300
         }
+//        completion: { _ in                                            // black View 그대로 존재, 투명하게 처리만 됨
+//            self.blackView.removeFromSuperview()                      // Completion을 통해 삭제 가능
+//            self.tableView.removeFromSuperview()
+//        }                                                             // 메모리 차이가 아주 미미하게 남
     }
     
     // MARK: - Functinos
@@ -58,16 +85,17 @@ class ActionSheetLauncher: NSObject {
         blackView.frame = window.frame
         
         window.addSubview(tableView)
-        tableView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: 300)
+        let height = CGFloat(3 * 60) + 100                                             // Row Height = 60
+        tableView.frame = CGRect(x: 0, y: window.frame.height, width: window.frame.width, height: height)
         
         UIView.animate(withDuration: 0.5) {
             self.blackView.alpha = 1
-            self.tableView.frame.origin.y -= 300
+            self.tableView.frame.origin.y -= height
         }
     }
     
     func configureTableView() {
-        tableView.backgroundColor = .systemPurple
+        tableView.backgroundColor = .white
         tableView.delegate = self
         tableView.dataSource = self
         tableView.rowHeight = 60
@@ -75,7 +103,7 @@ class ActionSheetLauncher: NSObject {
         tableView.layer.cornerRadius = 5
         tableView.isScrollEnabled = false
         
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.register(ActionSheetCell.self, forCellReuseIdentifier: reuseIdentifier)
     }
 }
 
@@ -87,7 +115,7 @@ extension ActionSheetLauncher: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! ActionSheetCell
         
         return cell
     }
@@ -96,5 +124,11 @@ extension ActionSheetLauncher: UITableViewDataSource {
 // MARK: - UITableViewDelegate
 
 extension ActionSheetLauncher: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return footerView
+    }
     
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 60
+    }
 }
---
+++ Jwitter/Source/View/ActionSheetCell.swift
@@ -0,0 +1,48 @@
+//
+//  ActionSheetCell.swift
+//  Jwitter
+//
+//  Created by 이지석 on 2022/02/25.
+//
+
+import UIKit
+
+class ActionSheetCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    private let optionImageView: UIImageView = {                        // Actino Sheet 아이콘 Image View
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = UIImage(named: "twitter_logo_blue")
+        return iv
+    }()
+    
+    private let titleLabel: UILabel = {                                 // Action Sheet 이름 Label
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = "Test Option"
+        return label
+    }()
+    
+    
+    // MARK: - Life Cycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(optionImageView)
+        optionImageView.centerY(inView: self)
+        optionImageView.anchor(left: leftAnchor,  paddingLeft: 8)
+        optionImageView.setDimensions(width: 36, height: 36)
+        
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: self)
+        titleLabel.anchor(left: optionImageView.rightAnchor, paddingRight: 12)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
